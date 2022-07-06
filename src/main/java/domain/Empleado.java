a2376fc4a42edbae61efb014fab3f4644683b027
cumbinar <cumbiweb@gmail.com>
2022-07-06 08:38:43 -0500

    get set class Empleado, and Override toString

--- src/main/java/domain/Empleado.java
+++ src/main/java/domain/Empleado.java
@@ -9,5 +9,40 @@ package domain;
  * @author educu
  */
 public class Empleado extends Persona{   //hereda de la clase Persona
+    private int idEmpleado;
+    private double sueldo;
+    private static int contadorEmpleado;
+
+    public Empleado(String nombre, double sueldo) {   //nombre de la clase persona y sueldo de Empleado
+        super(nombre);
+        this.idEmpleado = ++ Empleado.contadorEmpleado;  // variable local
+        this.sueldo = sueldo;    
+    }
+
+    public int getIdEmpleado() {
+        return this.idEmpleado;
+    }
+
+    public double getSueldo() {
+        return sueldo;
+    }
+
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Empleado{");
+        sb.append("idEmpleado=").append(idEmpleado);
+        sb.append(", sueldo=").append(sueldo);
+        sb.append('}');
+        return sb.toString();
+    }
+    
+    
+    
+    
     
 }
