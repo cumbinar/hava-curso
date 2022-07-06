2ed62bd83209d1ca46c543df985ba6309db6fb7e
cumbinar <cumbiweb@gmail.com>
2022-07-06 07:59:20 -0500

    se crea la clase TestHerencia

---
+++ src/main/java/test/TestHerencia.java
@@ -0,0 +1,15 @@
+
+package test;
+
+import domain.Empleado;
+
+/**
+ *
+ * @author educu
+ */
+public class TestHerencia {
+    public static void main(String[] args) {
+        Empleado empleado1 = new Empleado();
+        System.out.println("empleado1 = " + empleado1);
+    }
+}
