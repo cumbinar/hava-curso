1999b29c078cd3e392df613d7a6e895c7fff067e
cumbinar <cumbiweb@gmail.com>
2022-07-07 16:04:42 -0500

    refactorize Herencia proyect

--- src/main/java/domain/Cliente.java
+++ src/main/java/domain/Cliente.java
@@ -20,7 +20,42 @@ public class Cliente extends Persona{
     //se inicializan los atributos propios y los de la clase padre
     public Cliente(Date fechaRegistro, boolean vip,String nombre,char genero, int edad, String direccion ){
         super(nombre, genero, edad, direccion);
-        
+        this.idCliente = ++ Cliente.contadorCliente;
+        this.fechaRegistro = fechaRegistro;
+        this.vip = vip;
 }
+
+    public int getIdCliente() {
+        return idCliente;
+    }
+
+    public Date getFechaRegistro() {
+        return fechaRegistro;
+    }
+
+    public void setFechaRegistro(Date fechaRegistro) {
+        this.fechaRegistro = fechaRegistro;
+    }
+
+    public boolean isVip() {
+        return vip;
+    }
+
+    public void setVip(boolean vip) {
+        this.vip = vip;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Cliente{");
+        sb.append("idCliente=").append(idCliente);
+        sb.append(", fechaRegistro=").append(fechaRegistro);
+        sb.append(", vip=").append(vip);
+        sb.append(", ").append(super.toString());
+        sb.append('}');
+        return sb.toString();
+    }
+    
     
 }
--- src/main/java/test/TestHerencia.java
+++ src/main/java/test/TestHerencia.java
@@ -1,7 +1,9 @@
 
 package test;
 
+import domain.Cliente;
 import domain.Empleado;
+import java.util.Date;
 
 /**
  *
@@ -11,5 +13,8 @@ public class TestHerencia {
     public static void main(String[] args) {
         Empleado empleado1 = new Empleado("Cumbi ", 500000);
         System.out.println("empleado1 = " + empleado1);
+        
+        Cliente cliente = new Cliente(new Date(), true, "Eduardo",'F', 62, "carrera 27 calle 9");
+        System.out.println("cliente = " + cliente); //imprime los atributos de la clase hijo y clase padre
     }
 }
